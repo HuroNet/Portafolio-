425f0dfa9424c66b22293ec4e9d654f9c5ffc840
Huron <charlie.walls97@gmail.com>
2024-05-02 08:20:00 -0500

    update

--- src/componentes/Hero/Home.jsx
+++ src/componentes/Hero/Home.jsx
@@ -38,159 +38,84 @@ import pdf from "../imagenes/CV.pdf";
 const Home = () => {
   return (
     <>
-      <section className="hero" id="home">
-        <div className="container f_flex top">
-          <div className="left top">
-            <h3>WELCOME TO MY SELF</h3>
-            <h1>
-              Hi, I'm <span> Carlos Paredes</span>
-            </h1>
-            <h2>
-              a
-              <span>
-                <Typewriter
-                  words={[" IT Engineer", " Developer"]}
-                  loop
-                  cursor
-                  cursorStyle="_"
-                  typeSpeed={70}
-                  deleteSpeed={50}
-                  delaySpeed={1000}
-                />
-              </span>
-            </h2>
-            <div className="CV">
-              <button className="CV1 btn_shadow">
-                <a href={pdf} download>
-                  Download CV
-                </a>
-              </button>
-            </div>
-            <p>
-              I am a newly graduated programmer, I have a good experience in
-              programming. During my time as a student, I developed projects
-              related to software development. I've always been the type of
-              person to take your downsides and turn them into upsides, so I'm a
-              firm believer that there's always a way to overcome every
-              obstacle.
-            </p>{" "}
-            <div className="hero_btn d_flex">
-              <div className="">
-                <h4> FIND WITH ME</h4>
-                <div className="button1">
-                  <button className="btn_shadow">
-                    <a href="https://www.instagram.com/andres_walls_/?hl=es-la">
-                      <i>
-                        <FontAwesomeIcon icon={faInstagram} />
-                      </i>
-                    </a>
-                  </button>
-                  <button className="btn_shadow">
-                    <a href="http://wa.me/5930984508977">
-                      <i>
-                        <FontAwesomeIcon icon={faWhatsapp} />
-                      </i>
-                    </a>
-                  </button>
+      <div className="welcome" id="home">
+        <div className="perfil">
+          <h3>WELCOME TO MY SELF</h3>
+          <h1>
+            Hi, I'm <span> Carlos Paredes</span>
+          </h1>
+          <h2>
+            a
+            <span>
+              <Typewriter
+                words={[" IT Engineer", " Developer"]}
+                loop
+                cursor
+                cursorStyle="_"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+              />
+            </span>
+          </h2>
 
-                  <button className="btn_shadow">
-                    <a href="https://www.linkedin.com/in/carlos-paredes-4b1871259/">
-                      <i>
-                        <FontAwesomeIcon icon={faLinkedinIn} />
-                      </i>
-                    </a>
-                  </button>
-
-                  <button className="btn_shadow">
-                    <a href="https://github.com/Carlos-An20?tab=repositories">
-                      <i>
-                        <FontAwesomeIcon icon={faGithub} />
-                      </i>
-                    </a>
-                  </button>
-                </div>
-              </div>
-              <div className="">
-                <h4>BEST SKILL ON</h4>
-                <div className="button2">
-                  <button className="btn_shadow">
-                    <img src={skill1} alt="" />
-                  </button>{" "}
-                  <button className="btn_shadow">
-                    <img src={skill3} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill2} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill9} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill5} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill6} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill7} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill8} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill10} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill11} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill12} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill13} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill14} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill15} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill16} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill18} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill20} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill22} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill21} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill19} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill17} alt="" />
-                  </button>
-                  <button className="btn_shadow">
-                    <img src={skill4} alt="" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="right">
-            <div className="right_img">
-              <img src={hero} alt="" />
-            </div>
-          </div>
+          <button className="">
+            <a href={pdf} download>
+              Resume
+            </a>
+          </button>
+          <p>
+            I am a <span>Full-stack</span> developer specialized in application
+            and website development. My passion is creating innovative digital
+            solutions that provide unique experiences to users. With one year of
+            experience in the industry, I have worked on projects of varying
+            scale. My approach focuses on understanding user needs and
+            translating them into intuitive and efficient products. I use modern
+            technologies and agile methodologies to ensure quality and timely
+            delivery of projects.
+          </p>
         </div>
-      </section>
+
+        <div className="foto">
+          <img src={hero} />
+        </div>
+      </div>
+
+      <div className="findme hero_btn d_flex">
+        <h4> FIND WITH ME</h4>
+        <div className="button1">
+          <button className="btn_shadow">
+            <a href="https://www.instagram.com/andres_walls_/?hl=es-la">
+              <i>
+                <FontAwesomeIcon icon={faInstagram} />
+              </i>
+            </a>
+          </button>
+          <button className="btn_shadow">
+            <a href="http://wa.me/5930984508977">
+              <i>
+                <FontAwesomeIcon icon={faWhatsapp} />
+              </i>
+            </a>
+          </button>
+
+          <button className="btn_shadow">
+            <a href="https://www.linkedin.com/in/carlos-paredes-4b1871259/">
+              <i>
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </i>
+            </a>
+          </button>
+
+          <button className="btn_shadow">
+            <a href="https://github.com/Carlos-An20?tab=repositories">
+              <i>
+                <FontAwesomeIcon icon={faGithub} />
+              </i>
+            </a>
+          </button>
+        </div>
+      </div>
     </>
   );
 };
